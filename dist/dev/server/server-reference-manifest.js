085d5f13125b9b0dcc4b9d1cc2cd270ec1d741fb
Angela Fei <cr.angela.fei@gmail.com>
2026-05-18 15:39:00 +0800

    update .gitignore and remove dist/ folder

--- dist/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"dpWcUux1FI0nRATfIZvqQGZsTS8DkSxsZ7OS4eQC5II=\"\n}"
--- dist/dev/static/chunks/app_page_0fg.33-.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_0a~3irp._.js","static/chunks/node_modules_next_0aj6m7e._.js"],
-    source: "dynamic"
-});
